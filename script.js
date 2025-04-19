a21a2bd009dad41f9705f396a08f221dcf37216d
Ayusha200333 <ayushawijerathna@gmail.com>
2025-04-19 23:32:58 +0530

    add js

--- script.js
+++ script.js
@@ -1,64 +1,65 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active')
+}
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
+        let offset = sec.offsetTop -150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach.apply(links =>{
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
-            })
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
         }
     });
-}
-    menuIcon.onclick = () => {
-        menuIcon.classList.toggle('bx-x');
-        navbar.classList.toggle('active');
-    }
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+
+ScrollReveal({
+    distance: '80px',
+    duration:2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content,heading,.skills h1,.assignment h1,.services h1,.projects h1',{origin: 'top'});
+ScrollReveal().reveal('.home-img, .portfolio-box, .contact form,.container,.project-container' ,{origin: 'button'});
+ScrollReveal().reveal('.home-content h1, .about-img,.services-container', {origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content',{origin:'right'});
+ScrollReveal().reveal('.footer',{origin:'bottom'});
+
+
+const  typed = new Typed('.multiple-text',{
+    strings:['Frontend Developer' , 'Web Designer'],
+    typeSpeed:70,
+    backSpeed:70,
+    backDelay:1000,
+    loop:true,
+});
 
 
 
-//
-// let menu = document.querySelector('#menu-icon');
-// let menu = document.querySelector('#menu-icon');
-//
-// let navbar = document.querySelector('.navbar');
-//
-//
-// menu.onclick = () => {
-//    menu.classList.toggle('bx-x');
-//    navbar.classList.toggle('active');
-// }
-// window.onscroll = () => {
-//     menu.classList.remove('bx-x');
-//     navbar.classList.remove('active');
-// }
-//
-// const typed = new Typed('.multiple-text', {
-//     strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
-//     typeSpeed:50,
-// });
 
 
 
 
-//
-// let menu = document.querySelector('#menu-icon');
-// let navbar = document.querySelector('.navbar');
-//
-// menu.onclick = () => {
-//     menu.classList.toggle('bx-x');
-//     navbar.classList.toggle('active');
-// }
-//
-// window.onscroll = () => {
-//     menu.classList.remove('bx-x');
-//     navbar.classList.remove('active');
-// }
+
+
+
+
+
